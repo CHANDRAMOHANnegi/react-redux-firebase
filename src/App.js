c9b04f794788c1fc87b1a89a1ffe03043692d837
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2020-01-05 19:07:25 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
-import React, { Component } from 'react';
-import Navigation from './components/Navigation';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
-import DashboardContainer from './containers/DashboardContainer';
-import Login from './containers/LoginContainer';
-import Register from './containers/RegisterContainer';
-import "./App.css"
-import { verifyAuth } from './Store/Actions/Auth';
-import { logoutUser } from './Store/Actions/Auth';
-import Profile from './components/Profile';
-import ProtectedRoute from './ProtectedRoute';
-class App extends Component {
-  state = {
-    authenticated: false,
-  };
-  componentDidMount() {
-    this.props.verifyAuth();
-  }
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-  render() {
-    const { logoutUser, isVerifying,isAuthenticated } = this.props
-    return (
-      <Router >
-        <React.Fragment>
-          <Navigation authenticated={isAuthenticated} logoutUser={logoutUser} />
-          <Switch>
-            <ProtectedRoute path="/profile" component={Profile} isVerifying={isVerifying} isAuthenticated={isAuthenticated} />
-            <ProtectedRoute path="/dashboard" component={DashboardContainer} isVerifying={isVerifying} isAuthenticated={isAuthenticated} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route path="/" component={DashboardContainer} />
-            <Route path="*" component={() => "404 NOT FOUND"} />
-          </Switch>
-        </React.Fragment>
-      </Router>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-const mapStateToProps = (state) => {
-  return {
-    isLoggingIn: state.auth.isLoggingIn,
-    loginError: state.auth.loginError,
-    isAuthenticated: state.auth.isAuthenticated,
-    isVerifying: state.auth.isVerifying
-  };
-}
-const mapDispatchTProps = {
-  verifyAuth: verifyAuth, logoutUser
-}
-
-export default connect(mapStateToProps, mapDispatchTProps)(App);
-
-
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reduxThunk from 'redux-thunk';
-import reducers from './Store/reducers';
-import { Provider } from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-ReactDOM.render(<Provider store={store}><App /></Provider>,
-    document.getElementById('root'));
-// console.log(arguments);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
