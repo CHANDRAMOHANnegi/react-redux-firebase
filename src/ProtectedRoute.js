0728ddc95fcf0fba75315edfbaedc44a51d04108
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2020-01-05 19:01:50 +0530

    make it better

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+  return <Route {...rest} render={(props) =>
+    isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }} />
+  } />
+}
+
+export default ProtectedRoute;
