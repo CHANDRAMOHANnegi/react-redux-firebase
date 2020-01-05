51296ddb3a18219db9b4fede7018bd78f22637c3
CHANDRAMOHANnegi <chandermohan.negi@qtsolv.com>
2020-01-05 18:40:23 +0530

    make it better

---
+++ src/Store/Actions/AlgoliaSearchAction.js
@@ -0,0 +1,19 @@
+
+import { SEARCH_RESULT_SUCCESS, REQUEST_SEARCH_PENDING, SEARCH_ERROR } from '../ActionType/AlgoliaSearchActionType';
+import axiosService from '../../Inits/Axios';
+
+export const search = (searchType, query) => async (dispatch, getState) => {
+    dispatch({ type: REQUEST_SEARCH_PENDING })
+    if(!searchType)searchType="search";
+    const APIURL = `http://hn.algolia.com/api/v1/${searchType}?query=${query}`;
+    await axiosService.get(APIURL)
+        .then((response) => {
+            // console.log('search', response)
+            dispatch({ type: SEARCH_RESULT_SUCCESS, payload: response.data });
+        }).catch((error) => {
+            console.log(error)
+            if (error.response) {
+                dispatch({ type: SEARCH_ERROR, payload: error.response.data });
+            }
+        });
+}
