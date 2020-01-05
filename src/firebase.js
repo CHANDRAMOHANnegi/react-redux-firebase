6ae2f552792fbeba37c7f32f225b6ba0eadad666
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2020-01-06 01:57:52 +0530

    done

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWCTdw9ggqnLcFtOxqgLOdN-H87Lbz4zM",
-  authDomain: "housing-1561367427221.firebaseapp.com",
-  databaseURL: "https://housing-1561367427221.firebaseio.com",
-  projectId: "housing-1561367427221",
-  storageBucket: "housing-1561367427221.appspot.com",
-  messagingSenderId: "757685336293",
-  appId: "1:757685336293:web:da9ddd8faefe7daa503c0c",
-  measurementId: "G-FX0Y9QB7FF"
+  apiKey: " - ",
+  authDomain: "housing- .firebaseapp.com",
+  databaseURL: "https://housing- .firebaseio.com",
+  projectId: "housing- ",
+  storageBucket: "housing- .appspot.com",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: "G- B7FF"
 };
 
 firebase.initializeApp(firebaseConfig);
